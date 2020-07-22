e3c9a4b685a1bbfc4b5bb0e48bc92a761b3c78c8
Pooria Soltani <pooria.ms@gmail.com>
2020-07-22 13:19:58 +0430

    Modifications for the script to work with different crawler engines.

--- crawlers/run_tasks.py
+++ crawlers/run_tasks.py
@@ -1,12 +1,21 @@
-from config import ShanghaiConfig
-from crawlers.shanghai_crawler import ShanghaiCrawler
+from config import BaseConfig, ShanghaiConfig, THEConfig
+from crawlers import ShanghaiCrawler, THECrawler
+
+
+def engine_select(engine: str):
+    if engine == "Shanghai":
+        return (ShanghaiConfig, ShanghaiCrawler)
+    if engine == "THE":
+        return (THEConfig, THECrawler)
 
 
 if __name__ == "__main__":
-    for page in ShanghaiConfig.URLS:
-        if not page.get('crawl'):
-            continue
-        p = ShanghaiCrawler(
-            page["url"], page["year"], page["field"], page["subject"]
-        )
-        p.crawl()
+    for engine in BaseConfig.CRAWLER_ENGINE:
+        config, crawler = engine_select(engine)
+        for page in config.URLS:
+            if not page.get("crawl"):
+                continue
+            p = crawler(
+                page["url"], page["year"], page["field"], page["subject"]
+            )
+            p.crawl()
