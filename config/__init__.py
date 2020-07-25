2064d8cf3879ce85504515a3f90fa0862306a64f
Pooria Soltani <pooria.ms@gmail.com>
2020-07-25 13:52:01 +0430

    Imported the new 'QSConfig' class.

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,4 @@
 from config.config import BaseConfig  # noqa
+from config.config import QSConfig  # noqa
 from config.config import ShanghaiConfig  # noqa
 from config.config import THEConfig  # noqa
