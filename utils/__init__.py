2fc1e108a013ea725e2b84b2b9471a3334e34e18
Pooria Soltani <pooria.ms@gmail.com>
2020-08-01 00:25:09 +0430

    Created some helper scripts.

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from utils.country_names import country_names  # noqa
+from utils.get_row import get_row  # noqa
+from utils.nullify import nullify  # noqa
+from utils.whole_csv import whole_csv  # noqa
---
+++ utils/whole_csv.py
@@ -0,0 +1,20 @@
+import csv
+import io
+from pathlib import Path
+from typing import Dict, List
+
+
+def whole_csv(
+    file_path: Path, key: str, encoding: str = "utf-8", delimiter: str = ","
+) -> Dict[str, List[Dict[str, str]]]:
+    output: Dict[str, List[Dict[str, str]]] = {}
+    with io.open(file_path, "r", encoding=encoding) as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=delimiter)
+        for row in reader:
+            value = row.pop(key)
+            if value in output:
+                output[value].append(dict(row))
+            else:
+                output[value] = [dict(row)]
+
+    return output
