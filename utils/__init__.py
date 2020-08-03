ae9971bc9b51fe0b74c8229816e1efcf33b80aec
Pooria Soltani <pooria.ms@gmail.com>
2020-08-04 01:44:55 +0430

    Imported the 'ranking_process' function.

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,7 @@ from utils.country_names import country_names  # noqa
 from utils.get_row import get_row  # noqa
 from utils.metrics_process import metric_types  # noqa
 from utils.nullify import nullify  # noqa
+from utils.ranking_process import ranking_process  # noqa
 from utils.text_process import text_process  # noqa
 from utils.value_process import value_process  # noqa
 from utils.whole_csv import whole_csv  # noqa
