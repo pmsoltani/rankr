79be5456c1ada337fb7cd501e73abf592f1ea419
Pooria Soltani <pooria.ms@gmail.com>
2020-08-04 03:28:08 +0430

    Added a new script to store the rankings data into DB.

---
+++ db_rankings.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import List
+
+from config import DBConfig
+from rankr.db_models import SessionLocal
+from utils import ranking_process
+
+ranking_systems = DBConfig.RANKINGS["ranking_systems"]
+
+for system in ranking_systems:
+    dir_path: Path = DBConfig.MAIN_DIR / system
+    if not dir_path.exists():
+        continue
+
+    files: List[Path] = sorted(list(dir_path.iterdir()))
+    for cnt, file in enumerate(files, start=1):
+        if file.suffix != ".csv":
+            continue
+
+        # ranking_info = dict(
+        #     zip(
+        #         ["Ranking System","Ranking Type","Year","Field","Subject"],
+        #         file.stem.split("_"),
+        #     )
+        # )
+
+        print(f"Processing file ({cnt}/{len(files)}): {file.stem}")
+        try:
+            db = SessionLocal()
+            institutions_list = ranking_process(db, file)
+            if institutions_list:
+                db.add_all(institutions_list)
+                db.commit()
+        finally:
+            db.close()
