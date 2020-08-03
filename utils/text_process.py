8bdc5f5c36bd40a181739c8a810b19d2490062f9
Pooria Soltani <pooria.ms@gmail.com>
2020-08-03 13:27:41 +0430

    Successor to the 'vacuum' function inside the crawler modules.

---
+++ utils/text_process.py
@@ -0,0 +1,14 @@
+import re
+
+
+def text_process(text: str) -> str:
+    """Cleans the input text for further processing
+
+    Args:
+        text (str): Input text
+
+    Returns:
+        str: Clean text
+    """
+    parts = re.findall(r"[a-zA-Z0-9_.:/\&]+", text)
+    return " ".join(parts)
