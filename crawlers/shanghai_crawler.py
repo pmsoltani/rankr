b8c16cc702bf3eea1a7957dd9371db5216cf6515
Pooria Soltani <pooria.ms@gmail.com>
2020-08-05 02:42:52 +0430

    Simplified the process of 'ShanghaiCrawler'.

--- crawlers/shanghai_crawler.py
+++ crawlers/shanghai_crawler.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-from typing import List, Tuple
+from typing import Dict, List
 
+import requests
+from bs4 import BeautifulSoup
 from furl import furl
 
 from config import ShanghaiConfig
@@ -10,54 +12,60 @@ from utils import text_process
 
 class ShanghaiCrawler(CrawlerMixin, ShanghaiConfig):
     def __init__(self, url: str, **kwargs):
-        self.urls = [url]
-        self.use_js = False
-        self.merge_on_cols = []
+        self.url = url
         self.header_group_keyword = "institution"
 
         super().__init__(**kwargs)
 
-    def _get_tbl(self) -> Tuple[List[str], List[List[str]]]:
+    def _get_page(self) -> BeautifulSoup:
+        """Requests a page for data extraction.
+
+        Raises:
+            ConnectionError: If the request is not successful
+
+        Returns:
+            BeautifulSoup: The downloaded page
+        """
+        page = requests.get(self.url, headers=self.headers)
+        if page.status_code != 200:
+            raise ConnectionError(f"Error getting page: {self.url}")
+
+        self.page = BeautifulSoup(page.content, "html.parser")
+        return self.page
+
+    def _get_tbl(self) -> List[Dict[str, str]]:
         """Finds the ranking table within the page and extracts its data
 
         Returns:
-            Tuple[List[str], List[List[str]]]: Table headers and content
+            List[Dict[str, str]]: Table data as a list of dictionaries
         """
-        self.tbl_headers: List[List[str]] = []
-        self.tbl_contents: List[List[List[str]]] = []
-        for page in self.pages:
-            tbl = page.find("table", attrs={"id": "UniversityRanking"})
+        self.processed_data: List[List[str]] = []
 
-            tbl_headers: List[str] = [h.text for h in tbl.find_all("th")]
-            tbl_headers = self._clean_headers(tbl_headers)
-            self.tbl_headers.append(tbl_headers)
+        tbl = self.page.find("table", attrs={"id": "UniversityRanking"})
+        tbl_headers = self._clean_headers([h.text for h in tbl.find_all("th")])
 
-            tbl_contents: List[List[str]] = []
-            for row in tbl.find_all("tr"):
-                values = []
-                for val in row.find_all("td"):
-                    if val.find("img"):
-                        country = Path(val.find("img")["src"]).stem
-                        country = ShanghaiCrawler.country_name_mapper(
-                            text_process(country)
-                        )
-                        values.append(country)
-                        continue
-                    if val.find("a") and val.text:
-                        url = (
-                            furl(ShanghaiConfig.BASE_URL)
-                            / val.find("a")["href"]
-                        )
-                        values.append(url.url)
-                    values.append(val.text)
+        for row in tbl.find_all("tr"):
+            values = []
+            for val in row.find_all("td"):
+                if val.find("img"):
+                    country = Path(val.find("img")["src"]).stem
+                    country = ShanghaiCrawler.country_name_mapper(
+                        text_process(country)
+                    )
+                    values.append(country)
+                    continue
+                if val.find("a") and val.text:
+                    url = furl(ShanghaiConfig.BASE_URL) / val.find("a")["href"]
+                    values.append(url.url)
+                values.append(val.text)
 
-                if values:
-                    tbl_contents.append([v.strip() for v in values])
-            self.tbl_contents.append(tbl_contents)
+            if values:
+                values = dict(zip(tbl_headers, [v.strip() for v in values]))
+                self.processed_data.append({**values, **self.ranking_info})
 
-        if not self.tbl_headers or not self.tbl_contents:
+        if not tbl_headers or not self.processed_data:
             raise ConnectionError("Error getting page!")
-        return (self.tbl_headers, self.tbl_contents)
+        return self.processed_data
 
     def _clean_headers(self, headers: List[str]) -> List[str]:
         """Cleans a list of headers
@@ -68,16 +76,16 @@ class ShanghaiCrawler(CrawlerMixin, ShanghaiConfig):
         new_headers = []
 
         for h in headers:
-            h = text_process(h)
-            if self.header_group_keyword in h.lower():
+            h = text_process(h).lower()
+            if self.header_group_keyword in h:
                 new_headers.extend(["URL", "Institution", "Country"])
 
-            if ShanghaiCrawler.FIELDS.get(h):
-                new_headers.append(ShanghaiCrawler.FIELDS.get(h))
+            if ShanghaiConfig.FIELDS.get(h):
+                new_headers.append(ShanghaiConfig.FIELDS.get(h))
 
-            if h.startswith("Score on"):
-                tmp = h.replace("Score on", "").strip().split(" ")
-                tmp = [t for t in tmp if t.strip()]
+            if h.startswith("score on"):
+                tmp = h.replace("score on", "").strip().split(" ")
+                tmp = [ShanghaiConfig.FIELDS[t] for t in tmp if t.strip()]
                 new_headers.extend(tmp)
 
         return new_headers
