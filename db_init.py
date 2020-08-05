7ec9e140eb164e15fe0fb400d335351d480e27a0
Pooria Soltani <pooria.ms@gmail.com>
2020-08-05 21:13:40 +0430

    Improved the scripts with adequate messages.

--- db_init.py
+++ db_init.py
@@ -4,8 +4,13 @@ from config import DBConfig
 from rankr.db_models import Base, engine
 
 
-if not database_exists(engine.url):
-    encoding = "utf8mb4" if DBConfig.DIALECT == "mysql" else "utf8"
-    create_database(engine.url, encoding=encoding)
+try:
+    if not database_exists(engine.url):
+        encoding = "utf8mb4" if DBConfig.DIALECT == "mysql" else "utf8"
+        create_database(engine.url, encoding=encoding)
 
-Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
+    print("Successfully created the database!")
+except Exception as exc:
+    print("Error creating the database:")
+    print(type(exc), exc)
