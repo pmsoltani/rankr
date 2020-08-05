b8e1aab387d1838999d6d3b288a0639120030816
Pooria Soltani <pooria.ms@gmail.com>
2020-08-05 18:08:00 +0430

    Added a new table to better handle countries.

---
+++ rankr/db_models/country.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Integer, String
+
+from config import DBConfig
+from rankr.db_models.base import Base
+from rankr.db_models.institution import Institution
+
+
+class Country(Base):
+    __tablename__ = "country"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    country = Column(String(63), nullable=False, unique=True)
+    country_code = Column(String(2), nullable=False, unique=True)
+    region = Column(String(15), nullable=False)
+    sub_region = Column(String(63))
+
+    # Relationships
+    institutions: List[Institution] = relationship(
+        "Institution", back_populates="country"
+    )
+
+    def __init__(self, **kwargs):
+        kwargs = {k: v for k, v in kwargs.items() if k in self.__table__.c}
+        kwargs["country"] = DBConfig.COUNTRY_NAMES.get(
+            kwargs["country"].strip().lower(), kwargs["country"]
+        )
+        super().__init__(**kwargs)
+
+    def __repr__(self) -> str:
+        return f"{self.domain}: {self.name}"
