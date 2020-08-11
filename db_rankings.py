65c7b515f51d197c84650eb23c3ad6537f72efa4
Pooria Soltani <pooria.ms@gmail.com>
2020-08-11 07:57:43 +0430

    Modified the script to use the new 'fuzzy_matcher' function.

--- db_rankings.py
+++ db_rankings.py
@@ -2,10 +2,21 @@ from pathlib import Path
 from typing import List
 
 from config import DBConfig
-from rankr.db_models import SessionLocal
+from rankr.db_models import Institution, SessionLocal
 from utils import csv_export, csv_size, ranking_process
 
 ranking_systems = DBConfig.RANKINGS["ranking_systems"]
+
+db = SessionLocal()
+ili: List[Institution] = db.query(Institution).all()
+soup = {}
+for inst in ili:
+    try:
+        soup[inst.country.country][inst.soup] = inst.grid_id
+    except KeyError:
+        soup[inst.country.country] = {inst.soup: inst.grid_id}
+db.close()
+
 
 not_mached = []
 for system in ranking_systems:
@@ -21,11 +32,12 @@ for system in ranking_systems:
         try:
             db = SessionLocal()
             size = csv_size(file)
-            institutions_list, not_mached_list = ranking_process(db, file)
+            institutions_list, not_mached_list = ranking_process(db, file, soup)
             if len(institutions_list) + len(not_mached_list) != size:
                 raise ValueError("Some institutions may have been lost!")
             not_mached.extend(not_mached_list)
             if institutions_list:
+                pass
                 db.add_all(institutions_list)
                 db.commit()
         except ValueError as exc:
