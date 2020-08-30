1f9a6ee56d0e8f6b47e9483d1d1e7a0f7c601145
Pooria Soltani <pooria.ms@gmail.com>
2020-08-30 22:14:54 +0430

    Added a new schema to represent different entities.

--- rankr/schemas/__init__.py
+++ rankr/schemas/__init__.py
@@ -1,3 +1,4 @@
 """pydantic models (schemas) to validate/filter data prior sending to client"""
 
+from rankr.schemas.entity import EntitySchema  # noqa
 from rankr.schemas.ranking import RankingSchema  # noqa
