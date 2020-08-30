e286b9b8f5c55891d49c107805da9f24ff6bb317
Pooria Soltani <pooria.ms@gmail.com>
2020-08-30 21:13:32 +0430

    Added a new function to group list of objects/dicts by attribute/key.

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,7 @@ from utils.csv_size import csv_size  # noqa
 from utils.fuzzy_matcher import fuzzy_matcher  # noqa
 from utils.get_csv import get_csv  # noqa
 from utils.get_row import get_row  # noqa
+from utils.group_by import group_by  # noqa
 from utils.nullify import nullify  # noqa
 from utils.text_process import text_process  # noqa
 from utils.value_process import value_process  # noqa
