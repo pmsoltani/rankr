ccc156f29aab893e953169a3b24ae9143b3d2a06
Pooria Soltani <pooria.ms@gmail.com>
2020-08-30 21:14:32 +0430

    Added 2 new crud functions to get ranking systems and ranking tables.

--- rankr/crud/__init__.py
+++ rankr/crud/__init__.py
@@ -1,3 +1,4 @@
 """Classes and functions to put/process raw data to/from the database"""
 
+from rankr.crud.ranking import get_ranking_systems, get_ranking_table  # noqa
 from rankr.crud.ranking_process import ranking_process  # noqa
