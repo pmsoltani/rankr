4f5da82c8f44bc72859b3d6eea3142ef7f05ae27
Pooria Soltani <pooria.ms@gmail.com>
2020-08-30 20:44:42 +0430

    Modifications according to 5822543a.

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,8 +3,6 @@ from utils.csv_size import csv_size  # noqa
 from utils.fuzzy_matcher import fuzzy_matcher  # noqa
 from utils.get_csv import get_csv  # noqa
 from utils.get_row import get_row  # noqa
-from utils.metrics_process import metrics_process  # noqa
 from utils.nullify import nullify  # noqa
-from utils.ranking_process import ranking_process  # noqa
 from utils.text_process import text_process  # noqa
 from utils.value_process import value_process  # noqa
