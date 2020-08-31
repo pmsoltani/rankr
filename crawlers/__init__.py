81394b143f9c63f277d132860fe0ee7facf22ef2
Pooria Soltani <pooria.ms@gmail.com>
2020-08-31 17:44:01 +0430

    Added a new class to crawl wikipedia.

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,3 +1,4 @@
 from crawlers.qs_crawler import QSCrawler  # noqa
 from crawlers.shanghai_crawler import ShanghaiCrawler  # noqa
 from crawlers.the_crawler import THECrawler  # noqa
+from crawlers.wikipedia_crawler import WikipediaCrawler  # noqa
