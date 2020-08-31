40152909f0a0ffb0f3106fe426ce853dac839dc9
Pooria Soltani <pooria.ms@gmail.com>
2020-08-31 17:40:37 +0430

    Small modifications of imports.

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,4 +1,3 @@
-from crawlers.crawler_mixin import CrawlerMixin  # noqa
 from crawlers.qs_crawler import QSCrawler  # noqa
 from crawlers.shanghai_crawler import ShanghaiCrawler  # noqa
 from crawlers.the_crawler import THECrawler  # noqa
