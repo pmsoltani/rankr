86a9d0cad0fb50e73f76b468d9ac0b75830f8cc6
Pooria Soltani <pooria.ms@gmail.com>
2020-09-01 12:49:58 +0430

    Turned the script into a function.

--- cli/commands/db/db_institutions.py
+++ cli/commands/db/db_institutions.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import typer
 from sqlalchemy.orm import Session
 from tqdm import tqdm
 
@@ -16,75 +17,82 @@ from rankr.db_models import (
 )
 from utils import csv_size, get_csv, get_row, nullify
 
-# processing countries
-try:
-    db: Session = SessionLocal()
-    rows = get_row(DBConfig.MAIN_DIR / "countries.csv")
-    countries_list: List[Country] = []
-    for row in rows:
-        nullify(row)
-        countries_list.append(Country(**row))
-    if countries_list:
+
+def db_grid():
+    # processing countries
+    try:
+        typer.secho("Processing countries...", fg="green")
+        db: Session = SessionLocal()
+        rows = get_row(DBConfig.MAIN_DIR / "countries.csv")
+        countries_list: List[Country] = []
+        for row in rows:
+            nullify(row)
+            countries_list.append(Country(**row))
+
         db.add_all(countries_list)
         db.commit()
-finally:
-    db.close()
+    finally:
+        db.close()
 
+    attrs = ["addresses", "acronyms", "aliases", "labels", "links", "types"]
 
-attrs = ["addresses", "acronyms", "aliases", "labels", "links", "types"]
+    # processing institutions
+    try:
+        typer.secho("Processing institutions...", fg="green")
+        db: Session = SessionLocal()
 
-# processing institutions
-try:
-    print("Processing institutions ...")
-    db: Session = SessionLocal()
-
-    institution_attrs = [
-        get_csv(DBConfig.GRID_DATABASE_DIR / f"{attr}.csv", "grid_id")
-        for attr in attrs
-    ]
-    countries = {c.country: c for c in db.query(Country).all()}
-    rows = get_row(DBConfig.GRID_DATABASE_DIR / "institutes.csv")
-    row_count = csv_size(DBConfig.GRID_DATABASE_DIR / "institutes.csv")
-
-    pbar = tqdm(total=row_count)
-    for row in rows:
-        nullify(row)
-        address, acronym, alias, label, link, type = [
-            attr.get(row["grid_id"]) for attr in institution_attrs
+        institution_attrs = [
+            get_csv(DBConfig.GRID_DATABASE_DIR / f"{attr}.csv", "grid_id")
+            for attr in attrs
         ]
+        countries = {c.country: c for c in db.query(Country).all()}
+        rows = get_row(DBConfig.GRID_DATABASE_DIR / "institutes.csv")
+        row_count = csv_size(DBConfig.GRID_DATABASE_DIR / "institutes.csv")
 
-        soup = [row["name"]]
+        pbar = tqdm(total=row_count)
+        for row in rows:
+            nullify(row)
+            address, acronym, alias, label, link, type = [
+                attr.get(row["grid_id"]) for attr in institution_attrs
+            ]
 
-        if address:
-            country = DBConfig.country_name_mapper(address[0].pop("country"))
-            institution = Institution(**{**row, **address[0]})
-            institution.country = countries[country]
-            soup.append(country)
-        else:
-            institution = Institution(**row)
+            soup = [row["name"]]
 
-        if acronym:
-            institution.acronyms = [Acronym(**i) for i in acronym]
-            soup.extend(i["acronym"] for i in acronym)
-        if alias:
-            institution.aliases = [Alias(**i) for i in alias]
-            soup.extend(i["alias"] for i in alias)
-        if label:
-            institution.labels = [Label(**i) for i in label]
-            soup.extend(i["label"] for i in label)
-        if link:
-            institution.links = [Link(**i) for i in link]
-        if type:
-            institution.types = [Type(**i) for i in type]
+            if address:
+                country = DBConfig.country_name_mapper(
+                    address[0].pop("country")
+                )
+                institution = Institution(**{**row, **address[0]})
+                institution.country = countries[country]
+                soup.append(country)
+            else:
+                institution = Institution(**row)
 
-        institution.soup = " | ".join(i for i in soup)
+            if acronym:
+                institution.acronyms = [Acronym(**i) for i in acronym]
+                soup.extend(i["acronym"] for i in acronym)
+            if alias:
+                institution.aliases = [Alias(**i) for i in alias]
+                soup.extend(i["alias"] for i in alias)
+            if label:
+                institution.labels = [Label(**i) for i in label]
+                soup.extend(i["label"] for i in label)
+            if link:
+                institution.links = [Link(**i) for i in link]
+            if type:
+                institution.types = [Type(**i) for i in type]
 
-        db.add(institution)
-        pbar.update()
-    pbar.close()
+            institution.soup = " | ".join(i for i in soup)
 
-    print("Committing results to the database...")
-    db.commit()
-finally:
-    del institution_attrs
-    db.close()
+            db.add(institution)
+            pbar.update()
+        pbar.close()
+
+        typer.secho(
+            "Committing results to the DB. This can take several minutes.",
+            fg="green",
+        )
+        db.commit()
+    finally:
+        del institution_attrs
+        db.close()
