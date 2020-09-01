102224cf41233313f1f22226340ad4c6887cbeed
Pooria Soltani <pooria.ms@gmail.com>
2020-09-01 13:46:58 +0430

    Added the new command to the cli.

--- cli/commands/cmd_db.py
+++ cli/commands/cmd_db.py
@@ -1,8 +1,9 @@
 import typer
 
-from cli.commands.db import db_grid, db_init
+from cli.commands.db import db_grid, db_init, db_rankings
 
 
 cli = typer.Typer()
 cli.command(name="init")(db_init.db_init)
 cli.command(name="grid")(db_grid.db_grid)
+cli.command(name="rankings")(db_rankings.db_rankings)
