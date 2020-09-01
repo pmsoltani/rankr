04fa992380734c1dddccdb3ecb83e472672cb50d
Pooria Soltani <pooria.ms@gmail.com>
2020-09-01 17:07:14 +0430

    Added a new 'reset' command.

--- cli/commands/cmd_db.py
+++ cli/commands/cmd_db.py
@@ -7,3 +7,19 @@ cli = typer.Typer()
 cli.command(name="init")(db_init.db_init)
 cli.command(name="grid")(db_grid.db_grid)
 cli.command(name="rankings")(db_rankings.db_rankings)
+
+
+@cli.command()
+def reset(
+    ctx: typer.Context,
+    confirm: bool = typer.Option(
+        ...,
+        prompt="You're about to drop the database and re-create it. Continue?",
+        confirmation_prompt=True,
+    ),
+):
+    if not confirm:
+        raise typer.Abort()
+    ctx.invoke(db_init.db_init, force=True)
+    ctx.invoke(db_grid.db_grid)
+    ctx.invoke(db_rankings.db_rankings)
