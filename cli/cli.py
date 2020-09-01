32258dae3924f73ec72cc021eba3af17fd7995bd
Pooria Soltani <pooria.ms@gmail.com>
2020-09-01 18:41:37 +0430

    Modifications according to ca63edfd.

--- cli/cli.py
+++ cli/cli.py
@@ -4,5 +4,5 @@ from cli.commands import cmd_crawl, cmd_db
 
 
 cli = typer.Typer()
-cli.add_typer(cmd_crawl.cli, name="crawl")
+cli.command(name="crawl")(cmd_crawl.crawl)
 cli.add_typer(cmd_db.cli, name="db")
