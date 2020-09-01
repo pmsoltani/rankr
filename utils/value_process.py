pmsoltani/rankr

commit f895b7dee3ea09000694d75864164abf93f058ae

Added type hints.

utils/value_process.py
@@ -20,12 +20,12 @@ def value_process(
 
     # Dealing with ranges (e.g. Rank = "800-1000" -> "900", Rank = "47" -> "47")
     range_pattern = r"(\d+\.*\d*)[-־᠆‐‑‒–—―⁻₋−⸺⸻﹘﹣－:]*(\d+\.*\d*)*"
-    val = re.search(range_pattern, val)
+    matches: Optional[re.Match[str]] = re.search(range_pattern, val)
     try:
-        lower_bound, upper_bound = val.groups()
+        lower_bound, upper_bound = matches.groups()
         if upper_bound:
-            val = (Decimal(lower_bound) + Decimal(upper_bound)) / 2
-            val = str(int(val)) if value_type == "integer" else str(val)
+            total = (Decimal(lower_bound) + Decimal(upper_bound)) / 2
+            val = str(int(total)) if value_type == "integer" else str(val)
             return val
         return lower_bound
     except AttributeError:

utils/csv_export.py
@@ -8,7 +8,7 @@ def csv_export(
     file_path: Union[Path, str],
     data: List[Dict[str, str]],
     encoding: str = "utf-8",
-):
+) -> None:
     with io.open(file_path, "w", newline="", encoding=encoding) as csv_file:
         writer = csv.DictWriter(csv_file, data[0].keys(), quoting=csv.QUOTE_ALL)
         writer.writeheader()

utils/csv_size.py
@@ -1,10 +1,11 @@
 import csv
 import io
 from pathlib import Path
+from typing import Union
 
 
 def csv_size(
-    file_path: Path, encoding: str = "utf-8", delimiter: str = ","
+    file_path: Union[Path, str], encoding: str = "utf-8", delimiter: str = ","
 ) -> int:
     """Returns the number of rows in a .csv file
 

utils/get_csv.py
@@ -1,11 +1,14 @@
 import csv
 import io
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Union
 
 
 def get_csv(
-    file_path: Path, key: str, encoding: str = "utf-8", delimiter: str = ","
+    file_path: Union[Path, str],
+    key: str,
+    encoding: str = "utf-8",
+    delimiter: str = ",",
 ) -> Dict[str, List[Dict[str, str]]]:
     output: Dict[str, List[Dict[str, str]]] = {}
     with io.open(file_path, "r", encoding=encoding) as csv_file:

utils/get_row.py
@@ -1,11 +1,11 @@
 import csv
 import io
 from pathlib import Path
-from typing import Dict, Iterator
+from typing import Dict, Iterator, Union
 
 
 def get_row(
-    file_path: Path, encoding: str = "utf-8", delimiter: str = ","
+    file_path: Union[Path, str], encoding: str = "utf-8", delimiter: str = ","
 ) -> Iterator[Dict[str, str]]:
     """Yields a row from a .csv file
 

commit e635c6ba1ffa7efa523b7915d5b78d709a94588f

Fixed a bug caused by f895b7de.

utils/value_process.py
@@ -25,7 +25,7 @@ def value_process(
         lower_bound, upper_bound = matches.groups()
         if upper_bound:
             total = (Decimal(lower_bound) + Decimal(upper_bound)) / 2
-            val = str(int(total)) if value_type == "integer" else str(val)
+            val = str(int(total)) if value_type == "integer" else str(total)
             return val
         return lower_bound
     except AttributeError: