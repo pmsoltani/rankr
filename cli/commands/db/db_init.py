12ddb1493def619fb235e9cd2d90a8ecf8f95220
Pooria Soltani <pooria.ms@gmail.com>
2020-09-01 12:31:48 +0430

    Turned the script into a typer command.

--- cli/commands/db/db_init.py
+++ cli/commands/db/db_init.py
@@ -1,16 +1,23 @@
-from sqlalchemy_utils import create_database, database_exists
+import typer
+from sqlalchemy_utils import create_database, database_exists, drop_database
 
 from config import DBConfig
 from rankr.db_models import Base, engine
 
 
-try:
-    if not database_exists(engine.url):
-        encoding = "utf8mb4" if DBConfig.DIALECT == "mysql" else "utf8"
-        create_database(engine.url, encoding=encoding)
+def init(force: bool = False):
+    if force and database_exists(engine.url):
+        drop_database(engine.url)
+        typer.secho("Dropped the database!", fg="green")
 
-    Base.metadata.create_all(engine)
-    print("Successfully created the database!")
-except Exception as exc:
-    print("Error creating the database:")
-    print(type(exc), exc)
+    try:
+        if not database_exists(engine.url):
+            encoding = "utf8mb4" if DBConfig.DIALECT == "mysql" else "utf8"
+            create_database(engine.url, encoding=encoding)
+
+        Base.metadata.create_all(engine)
+        typer.secho("Successfully created the database!", fg="green")
+    except Exception as exc:
+        typer.secho("Error creating the database:", fg="red")
+        typer.secho(str(exc))
+        raise typer.Abort()
