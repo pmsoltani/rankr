495e1d27a7c22a10677ede33e395846eac837da0
Pooria Soltani <pooria.ms@gmail.com>
2020-09-01 16:34:35 +0430

    Improved the function

--- cli/commands/db/db_init.py
+++ cli/commands/db/db_init.py
@@ -1,5 +1,6 @@
 import typer
 from sqlalchemy_utils import create_database, database_exists, drop_database
+from typer.colors import CYAN, GREEN, RED
 
 from config import DBConfig
 from rankr.db_models import Base, engine
@@ -8,16 +9,15 @@ from rankr.db_models import Base, engine
 def db_init(force: bool = False):
     if force and database_exists(engine.url):
         drop_database(engine.url)
-        typer.secho("Dropped the database!", fg="cyan")
+        typer.secho("Dropped the database!", fg=CYAN)
 
     try:
-        if not database_exists(engine.url):
-            encoding = "utf8mb4" if DBConfig.DIALECT == "mysql" else "utf8"
-            create_database(engine.url, encoding=encoding)
+        encoding = "utf8mb4" if DBConfig.DIALECT == "mysql" else "utf8"
+        create_database(engine.url, encoding=encoding)
 
         Base.metadata.create_all(engine)
-        typer.secho("Successfully created the database!", fg="green")
+        typer.secho("Successfully created the database!", fg=GREEN)
     except Exception as exc:
-        typer.secho("Error creating the database:", fg="red")
-        typer.secho(str(exc))
+        typer.secho("Error creating the database:", fg=RED)
+        typer.secho(str(exc), fg=CYAN)
         raise typer.Abort()
