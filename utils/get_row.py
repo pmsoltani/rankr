ac1060d031763c0977a00b8cd02051fa3a7832c4
Pooria Soltani <pooria.ms@gmail.com>
2020-09-02 10:13:45 +0430

    📄 Added documentation.

--- utils/get_row.py
+++ utils/get_row.py
@@ -7,18 +7,20 @@ from typing import Dict, Iterator, Union
 def get_row(
     file_path: Union[Path, str], encoding: str = "utf-8", delimiter: str = ","
 ) -> Iterator[Dict[str, str]]:
-    """Yields a row from a .csv file
+    """Yields a row from a .csv file.
 
-    This simple function is used to yield a .csv file in 'file_path',
+    This simple function is used to yield the specified .csv file
     row-by-row, so as not to consume too much memory.
 
     Args:
-        file_path (Path): the path to the .csv file
-        encoding (str): encoding to be used when reading the .csv file
-        delimiter (str): the delimiter used in the .csv file
+        file_path (Union[Path, str]): The path to the .csv file
+        encoding (str, optional): The encoding to be used when reading
+        the .csv file. Defaults to "utf-8".
+        delimiter (str, optional): The delimiter used in the .csv file.
+        Defaults to ",".
 
     Yields:
-        row: a row of the .csv file as a dictionary
+        Iterator[Dict[str, str]]: A row of .csv file as a dictionary
     """
     with io.open(file_path, "r", encoding=encoding) as csv_file:
         reader = csv.DictReader(csv_file, delimiter=delimiter)
