bb6ae32158fb1175cd435f6064f00296f3d7aca3
Pooria Soltani <pooria.ms@gmail.com>
2020-09-02 09:44:10 +0430

    📄 Added documentation.

--- utils/text_process.py
+++ utils/text_process.py
@@ -2,7 +2,11 @@ import re
 
 
 def text_process(text: str) -> str:
-    """Cleans the input text for further processing
+    """Cleans the input text for further processing.
+
+    This function re-constructs the input string from the parts that
+    match a pre-defined pattern. The pattern only allows for a small
+    set of characters, making the output text free on unwanted string.
 
     Args:
         text (str): Input text
