07c081a6679c270c09100b84f921d7ca8f2468b9
Pooria Soltani <pooria.ms@gmail.com>
2020-09-04 10:02:50 +0430

    Added test and coverage commands to the CLI.

--- cli/cli.py
+++ cli/cli.py
@@ -1,8 +1,10 @@
 import typer
 
-from cli.commands import cmd_crawl, cmd_db
+from cli.commands import cmd_crawl, cmd_db, cmd_test
 
 
 cli = typer.Typer()
 cli.command(name="crawl")(cmd_crawl.crawl)
+cli.command(name="cov")(cmd_test.cov)
+cli.command(name="test")(cmd_test.test)
 cli.add_typer(cmd_db.cli, name="db")
---
+++ cli/commands/cmd_test.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import subprocess
+
+import typer
+
+from config import appc
+
+
+def test(
+    path: Path = typer.Argument(appc.ROOT_DIR),
+    capture: bool = typer.Option(True, help="Disable print statements?"),
+) -> int:
+    """Runs unit tests with pytest.
+
+    Args:
+        path (Path, optional): Test path. Defaults to root directory.
+        capture (bool, optional): Whether to suppress print statements
+        or not. Defaults to True.
+
+    Returns:
+        int: The return code of the command
+    """
+    capture_flag = "" if capture else "--capture=no"
+    cmd = f"pytest {capture_flag} {path}"
+    return subprocess.call(args=cmd, shell=True)
+
+
+def cov(path: Path = typer.Argument(appc.ROOT_DIR)) -> int:
+    """Runs a test coverage report.
+
+    Args:
+        path (Path, optional): Test coverage analysis path. Defaults to
+        root directory
+
+    Returns:
+        int: The return code of the command
+    """
+    cmd = f"pytest --cov-report term-missing --cov {path}"
+    return subprocess.call(args=cmd, shell=True)
