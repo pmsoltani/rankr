f67aee6cf24c7f5e0d578562671a6903b4b7f6db
Pooria Soltani <pooria.ms@gmail.com>
2020-09-04 10:45:54 +0430

    Imported the new cli commands.

--- cli/cli.py
+++ cli/cli.py
@@ -1,10 +1,16 @@
 import typer
 
-from cli.commands import cmd_crawl, cmd_db, cmd_test
+from cli.commands import cmd_crawl, cmd_db, cmd_flake8, cmd_start, cmd_test
 
 
 cli = typer.Typer()
 cli.command(name="crawl")(cmd_crawl.crawl)
+
+cli.command(name="flake8")(cmd_flake8.flake8)
+
+cli.command(name="start")(cmd_start.start)
+
 cli.command(name="cov")(cmd_test.cov)
 cli.command(name="test")(cmd_test.test)
+
 cli.add_typer(cmd_db.cli, name="db")
