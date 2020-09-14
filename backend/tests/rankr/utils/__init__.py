a363de0f30877a82207239f1e6bafc66efaaec4e
Pooria Soltani <pooria.ms@gmail.com>
2020-09-14 10:10:22 +0430

    Moved all tests to a single location. Modified the imports.

---
+++ backend/tests/rankr/utils/__init__.py
@@ -0,0 +1,7 @@
+from tests.rankr.utils.generate_data import (  # noqa
+    fake_institutions,
+    fake_rankings,
+    fill_countries,
+)
+from tests.rankr.utils.overrides import override_get_db  # noqa
+from tests.rankr.utils.base import engine, TestingSessionLocal  # noqa
