5334008d1af09202f8de7eb563d89c4a0017171e
Pooria Soltani <pooria.ms@gmail.com>
2021-03-10 13:45:02 +0330

    Removed old tests.

--- backend/tests/rankr/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from tests.rankr.utils.generate_data import (
-    fake_institutions,
-    fake_rankings,
-    fill_countries,
-)
-from tests.rankr.utils.overrides import override_get_db
-from tests.rankr.utils.base import engine, TestingSessionLocal
