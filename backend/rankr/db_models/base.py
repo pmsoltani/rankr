bc9ba70981d1583bd631d55423182037ee82fbdf
Pooria Soltani <pooria.ms@gmail.com>
2021-03-10 11:53:58 +0330

    ✨Added a new function + Refactoring.

--- backend/rankr/db_models/base.py
+++ backend/rankr/db_models/base.py
@@ -1,10 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import DatabaseError
+from sqlalchemy_utils import create_database
 
 from config import dbc
 
-engine = create_engine(dbc.DB_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(dbc.DB_URL, encoding=dbc.DB_ENCODING)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base(bind=engine)
+
+
+def validate_database():
+    try:
+        create_database(engine.url, encoding=dbc.DB_ENCODING)
+    except DatabaseError:
+        pass
