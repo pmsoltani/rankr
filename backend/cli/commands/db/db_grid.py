00d6e52769c8918a6f23b2158f6e28b95640a9a8
Pooria Soltani <pooria.ms@gmail.com>
2021-07-18 11:21:05 +0430

    🖥 Simplified the db_init command by using the GRIDCrawler class.

--- backend/cli/commands/db/db_grid.py
+++ backend/cli/commands/db/db_grid.py
@@ -1,31 +1,19 @@
 from contextlib import closing
 
 import typer
-from sqlalchemy.orm import Session
 from typer.colors import CYAN, RED
 
-from rankr.crud import country_process, institution_process
-from rankr import db_models as d
+from rankr import crawlers as c, db_models as d, repos as r
 
 
 def db_grid():
     """Populates the database with country & GRID data."""
     try:
-        db: Session
-        typer.secho("Processing countries...", fg=CYAN)
         with closing(d.SessionLocal()) as db:
-            db.add_all(country_process())
-            db.commit()
-
-        typer.secho("Processing institutions...", fg=CYAN)
-        with closing(d.SessionLocal()) as db:
-            countries = {c.country: c for c in db.query(d.Country).all()}
-            db.add_all(institution_process(countries=countries))
-            typer.secho(
-                "Committing results to the DB. This can take several minutes.",
-                fg=CYAN,
-            )
-            db.commit()
+            country_repo = r.CountryRepo(db)
+            institution_repo = r.InstitutionRepo(db)
+            grid_crawler = c.GRIDCrawler(country_repo, institution_repo)
+            grid_crawler.crawl()
     except Exception as exc:
         typer.secho("Error populating the database: {type(exc)}", fg=RED)
         typer.secho(str(exc), fg=CYAN)
