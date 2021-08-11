7e98b0bf3ab1713a859f6da8f7a5d850a8de2efd
Pooria Soltani <pooria.ms@gmail.com>
2021-08-11 14:03:24 +0430

    🗄 Added the fuzzy-matching functionality from 'ranking_process.py'.

--- backend/rankr/repos/institution_repo.py
+++ backend/rankr/repos/institution_repo.py
@@ -1,9 +1,12 @@
-from typing import List, Optional
+from typing import Dict, List, Optional, Tuple
 
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
+from config import dbc
 from rankr import db_models as d, schemas as s
 from rankr.repos.base_repo import BaseRepo
+from utils import fuzzy_matcher
 
 
 class InstitutionRepo(BaseRepo):
@@ -54,3 +57,50 @@ class InstitutionRepo(BaseRepo):
         return self._get_objects(
             search_query=search_query, offset=offset, limit=limit
         )
+
+    def match_institution(
+        self,
+        institution_name: str,
+        institution_url: str,
+        link_type: str,
+        country_name: str,
+        soup: Dict[str, Dict[str, str]],
+    ) -> Tuple[Optional[d.Institution], bool]:
+        institution_name = institution_name.strip().lower()
+        fuzzy_flag = False
+        db_institution: Optional[d.Institution] = None
+
+        # checking grid_id in manual matches
+        match = dbc.MATCHES.get(country_name, {}).get(institution_name)
+        if match:
+            db_institution = self._get_db_object(
+                flt=[d.Institution.grid_id == match]
+            )
+
+        # checking link with institution links
+        if not db_institution:
+            flt = [d.Link.link == institution_url, d.Link.type == link_type]
+            db_institution = self._get_db_object_by_relation(
+                join=d.Institution.links, flt=flt
+            )
+
+        # checking name with institution name
+        if not db_institution:
+            flt = [
+                func.lower(d.Institution.name) == institution_name,
+                d.Country.country == country_name,
+            ]
+            db_institution = self._get_db_object_by_relation(
+                join=d.Institution.country, flt=flt
+            )
+
+        # fuzzy-mataching of strings
+        if not db_institution:
+            match = fuzzy_matcher(institution_name, country_name, soup)
+            if match:
+                db_institution = self._get_db_object(
+                    flt=[d.Institution.grid_id == match]
+                )
+                fuzzy_flag = True
+
+        return db_institution, fuzzy_flag
