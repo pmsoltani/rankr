bbba8adafb5d73db4e669563235a25b5b8a877e4
Pooria Soltani <pooria.ms@gmail.com>
2021-08-19 00:20:30 +0430

    ⚙️ Improved database dependencies.

--- backend/rankr/api/dependencies/__init__.py
+++ backend/rankr/api/dependencies/__init__.py
@@ -1,4 +1,4 @@
-from rankr.api.dependencies.database import get_db
+from rankr.api.dependencies.database import get_db, get_repo
 from rankr.api.dependencies.entities import (
     check_entities,
     get_entity_type,
--- backend/rankr/api/dependencies/database.py
+++ backend/rankr/api/dependencies/database.py
@@ -1,14 +1,22 @@
-from typing import Generator
+from contextlib import closing
+from typing import Callable, Generator
 
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from rankr import db_models as d
+from rankr.repos.base import BaseRepo
 
 
 def get_db() -> Generator[Session, None, None]:
     """Yields a SQLAlchemy session for the CRUD operations."""
-    db: Session = d.SessionLocal()
-    try:
+    db: Session
+    with closing(d.SessionLocal()) as db:
         yield db
-    finally:
-        db.close()
+
+
+def get_repo(RepoClass) -> Callable:
+    def get_repo(db: Session = Depends(get_db)) -> BaseRepo:
+        return RepoClass(db)
+
+    return get_repo
