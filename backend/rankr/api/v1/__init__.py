3ce8b0b7ed726003ee773bc9b2e08143c7e5f8d7
Pooria Soltani <pooria.ms@gmail.com>
2021-08-19 00:26:49 +0430

    Relocated the imports.

--- backend/rankr/api/v1/__init__.py
+++ backend/rankr/api/v1/__init__.py
@@ -1,12 +1,1 @@
 """Routes, dependencies, and other essentials for a functioning API"""
-
-from fastapi import APIRouter
-
-from rankr.api.v1.routers.entity import router as entity_router
-from rankr.api.v1.routers.ranking import router as ranking_router
-
-
-router = APIRouter()
-
-router.include_router(entity_router, prefix="/entities", tags=["entities"])
-router.include_router(ranking_router, prefix="/rankings", tags=["rankings"])
--- backend/rankr/api/v1/routers/__init__.py
+++ backend/rankr/api/v1/routers/__init__.py
@@ -2,9 +2,11 @@
 
 from fastapi import APIRouter
 
+from rankr.api.v1.routers.institution import router as institution_router
 from rankr.api.v1.routers.ranking import router as ranking_router
 
 
 router = APIRouter()
 
-router.include_router(ranking_router, prefix="/ranking", tags=["ranking"])
+router.include_router(institution_router, prefix="/i", tags=["institution"])
+router.include_router(ranking_router, prefix="/r", tags=["ranking"])
