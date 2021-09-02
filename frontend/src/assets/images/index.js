4d1dc5dd49e25d158f9286a323fee9a167223789
Pooria Soltani <pooria.ms@gmail.com>
2021-09-02 19:57:55 +0430

    🖼 Added necessary icons and images.

---
+++ frontend/src/assets/images/index.js
@@ -0,0 +1,23 @@
+export { default as logo } from './logo.svg'
+export { default as landing } from './landing.svg'
+export { default as uniLogo } from './uniLogo.svg'
+
+export { default as homeDisabled } from './homeDisabled.svg'
+export { default as homeFill } from './homeFill.svg'
+export { default as institutionOutline } from './institutionOutline.svg'
+export { default as locationOutline } from './locationOutline.svg'
+
+export { default as gridInverse } from './gridInverse.svg'
+export { default as gridDisabled } from './gridDisabled.svg'
+
+export { default as qsColor } from './qsColor.svg'
+export { default as qsDisabled } from './qsDisabled.svg'
+
+export { default as shanghaiColor } from './shanghaiColor.svg'
+export { default as shanghaiDisabled } from './shanghaiDisabled.svg'
+
+export { default as theColor } from './theColor.svg'
+export { default as theDisabled } from './theDisabled.svg'
+
+export { default as wikipediaDisabled } from './wikipediaDisabled.svg'
+export { default as wikipediaInverse } from './wikipediaInverse.svg'
