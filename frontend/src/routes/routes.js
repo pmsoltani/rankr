ca8c4634e559aee526ac49423ccbed7bd4bb6a7d
Pooria Soltani <pooria.ms@gmail.com>
2021-09-07 21:04:59 +0430

    🛣 Added new routes

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,7 +1,10 @@
 const routes = {
   home: { url: '/', label: 'Home' },
+  institutionLogo: { url: '/i/logo', label: 'Institution Logo' },
   institutions: { url: '/i', label: 'Institutions' },
   rankings: { url: '/r', label: 'Rankings' },
+  rankingSystems: { url: '/r/systems', label: 'Ranking Systems' },
+  rankingTable: { url: '/r/table', label: 'Ranking Table' },
   search: { url: '/s', label: 'Search' },
   about: { url: '/about', label: 'About' },
   notFoundPage: { url: '*', label: 'Page not found' }
