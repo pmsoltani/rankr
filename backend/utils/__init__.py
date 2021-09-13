d93f9548bdca569eb0e947276d6960ab508bd0fe
Pooria Soltani <pooria.ms@gmail.com>
2021-09-13 22:24:18 +0430

    🗑 Removed unncessary module.

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -9,5 +9,4 @@ from utils.get_row import get_row
 from utils.group_by import group_by
 from utils.nullify import nullify
 from utils.str_export import str_export
-from utils.text_process import text_process
 from utils.value_process import value_process
