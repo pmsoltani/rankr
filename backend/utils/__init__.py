475c8d81b3ce0c2ebe79c72904698a7ad017d2f6
Pooria Soltani <pooria.ms@gmail.com>
2021-09-24 03:57:46 +0330

    🛠 Utility function to create redis cache keys.

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -8,5 +8,6 @@ from utils.get_json import get_json
 from utils.get_row import get_row
 from utils.group_by import group_by
 from utils.nullify import nullify
+from utils.redis_cache_key_builder import redis_cache_key_builder
 from utils.str_export import str_export
 from utils.value_process import value_process
