fd9bf2cb7cd265f8fcec376d60bade2155e803e6
Pooria Soltani <pooria.ms@gmail.com>
2021-09-24 03:57:10 +0330

    ⚙️ Added redis config.

--- backend/config/backend_config.py
+++ backend/config/backend_config.py
@@ -13,6 +13,14 @@ class BackendConfig(BaseConfig):
     BACKEND_PORT: int
     CORS_ORIGINS: Union[str, List[AnyHttpUrl]]
 
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_PASS: str
+    REDIS_DB: str
+    REDIS_URL: str = ""
+    REDIS_ENCODING: str = "utf8"
+    REDIS_CACHE_EXPIRES_AFTER: int = 2592000
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
@@ -22,3 +30,12 @@ class BackendConfig(BaseConfig):
         if isinstance(cors_origins, str):
             return [item.strip() for item in cors_origins.split(",")]
         return cors_origins
+
+    @validator("REDIS_URL", always=True)
+    def _db_url_value(cls, redis_url, values) -> str:
+        return (
+            "redis://:"
+            + f"{values['REDIS_PASS']}@"
+            + f"{values['REDIS_HOST']}:{values['REDIS_PORT']}"
+            + f"/{values['REDIS_DB']}"
+        )
